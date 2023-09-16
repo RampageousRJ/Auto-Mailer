20a3e562e23069289cc55d33056b4f5749def4aa
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-09-16 12:17:24 +0530

    Recaptcha Added

--- mailer/__init__.py
+++ mailer/__init__.py
@@ -9,6 +9,8 @@ app.config['MAIL_USERNAME'] = 'automailer.0123@gmail.com'
 app.config['MAIL_PASSWORD'] = 'whjq jjdo tfrv ksqy'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+app.config['RECAPTCHA_PUBLIC_KEY'] = "6LcyAbAnAAAAAGj1ZLz7ZqWVCqBuZAlvzLWS9pRK"
+app.config['RECAPTCHA_PRIVATE_KEY'] = "6LcyAbAnAAAAAPXGV_OzjOXHWvxu6ocLeO3XR7VV"
 mail = Mail(app)
 
 from mailer.routes import *
--- mailer/forms.py
+++ mailer/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm,RecaptchaField
 from wtforms import FileField,StringField,SubmitField,TextAreaField
 from wtforms.validators import DataRequired,Length
 from flask_wtf.file import FileRequired,FileAllowed
@@ -7,4 +7,5 @@ class UploadForm(FlaskForm):
     title = StringField("Enter Title ",validators=[DataRequired()])
     upload = FileField("Upload Excel File ",validators=[FileRequired(),FileAllowed(['xlsx'],'Excel Sheets Only!')])
     body = TextAreaField("Enter Body ",validators=[DataRequired()])
+    recaptcha = RecaptchaField()
     submit = SubmitField("Send Mail")
