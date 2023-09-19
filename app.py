c074343ac999c524a54eb345b6633de007599177
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2023-09-19 11:01:54 +0530

    Env Settings

--- app.py
+++ app.py
@@ -2,7 +2,3 @@ from mailer import app
 
 if __name__=="__main__":
     app.run(debug=True)
-    
-''' cd 'E:/Study/OneDrive - Manipal Academy of Higher Education/Coding/Mini-Projects/Auto-Mailer'
- source virt/Scripts/activate 
-  export FLASK_DEBUG=1 '''
--- mailer/__init__.py
+++ mailer/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_mail import Mail
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY']='7dbb1be0d140bd8f205c8d07'
+app.config['SECRET_KEY']=os.getenv("AUTOMAILER_SECRET_KEY")
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'automailer.0123@gmail.com'
-app.config['MAIL_PASSWORD'] = 'whjq jjdo tfrv ksqy'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_ID')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
-app.config['RECAPTCHA_PUBLIC_KEY'] = "6LcyAbAnAAAAAGj1ZLz7ZqWVCqBuZAlvzLWS9pRK"
-app.config['RECAPTCHA_PRIVATE_KEY'] = "6LcyAbAnAAAAAPXGV_OzjOXHWvxu6ocLeO3XR7VV"
+app.config['RECAPTCHA_PUBLIC_KEY'] = os.getenv('AUTOMAILER_PUBLIC_KEY')
+app.config['RECAPTCHA_PRIVATE_KEY'] = os.getenv('AUTOMAILER_PRIVATE_KEY')
 mail = Mail(app)
 
 from mailer.routes import *
