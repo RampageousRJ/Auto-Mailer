f10c7a0e7a03888e1d9ad7e0bfb3fb9349d0d1c6
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2024-07-13 21:31:23 +0530

    Added naming feature

--- mailer/forms.py
+++ mailer/forms.py
@@ -4,9 +4,9 @@ from wtforms.validators import DataRequired,Length
 from flask_wtf.file import FileRequired,FileAllowed
 
 class UploadForm(FlaskForm):
+    name = StringField("Enter Sender Name ",validators=[DataRequired()])
     title = StringField("Enter Title ",validators=[DataRequired()])
     upload = FileField("Upload Excel File ",validators=[FileRequired(),FileAllowed(['xlsx'],'Excel Sheets Only!')])
     body = TextAreaField("Enter Body ",validators=[DataRequired()])
-    recaptcha = RecaptchaField()
     submit = SubmitField("Send Mail")
     attachments = FileField("Attach Files",validators=[FileAllowed(['png','pdf','jpg','jpeg'],'Allowed: png,jpg,jpeg,pdf')])
