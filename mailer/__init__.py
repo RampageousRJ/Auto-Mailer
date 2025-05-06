f149fbf6feb30f9a9ea59062a5faf1b32a0b7ae0
Rishabh Jain <rishabh.jain1@learner.manipal.edu>
2025-05-06 21:20:15 +0530

    Logging and Sanitization

--- mailer/__init__.py
+++ mailer/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_mail import Mail
 from dotenv import load_dotenv
 import os
+import logging
 load_dotenv()
 
 app = Flask(__name__)
@@ -16,5 +17,15 @@ app.config['RECAPTCHA_PUBLIC_KEY'] = os.getenv('AUTOMAILER_PUBLIC_KEY')
 app.config['RECAPTCHA_PRIVATE_KEY'] = os.getenv('AUTOMAILER_PRIVATE_KEY')
 app.config['UPLOAD_FOLDER']=os.path.abspath(os.getcwd())+"\\mailer\\static\\attachments\\"
 mail = Mail(app)
+
+app_logger = logging.Logger('automailer')
+app_logger.setLevel(logging.DEBUG)
+
+if not app_logger.handlers:
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('[%(asctime)s] %(levelname)s: %(message)s')
+    console_handler.setFormatter(formatter)
+    app_logger.addHandler(console_handler)
 
 from mailer.routes import *
--- mailer/routes.py
+++ mailer/routes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-from mailer import app,mail
+from mailer import app,mail,app_logger
 from mailer.forms import *
 from flask import render_template,request,redirect,flash
 from flask_mail import Message
@@ -18,40 +18,46 @@ def validEmail(email_text):
 @app.route('/',methods=['GET','POST'])
 @app.route('/home',methods=['GET','POST'])
 def home():
-    res=[]
+    emails=[]
     form=UploadForm()
     if request.method=='POST':
         file_req = request.files['upload']
         attach = request.files['attachments']
         try:
             file = pd.read_excel(file_req)
-            file_name = secure_filename(attach.filename)
-            file_names = str(uuid.uuid1()) + "_" + file_name
-            if file_name:
-                attach.save(os.path.join(app.config['UPLOAD_FOLDER'], file_names))
+            sanitized_filepath = secure_filename(attach.filename)
+            attachment_filename = str(uuid.uuid1()) + "_" + sanitized_filepath
+            if sanitized_filepath:
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], attachment_filename)
+                attach.save(file_path)
+                if os.path.exists(file_path):
+                    app_logger.info("Attachment uploaded successfully!")
+                else:
+                    app_logger.error("Unable to upload attachment!")
             for index,row in file.iterrows():
                 for values in row:
                     if validEmail(values):
-                        res.append(values)
+                        emails.append(values)
                         break
-            l1=[]
-            for i in res:
-                if i not in l1:
-                    l1.append(i)
+            users=[]
+            for email in emails:
+                if email not in users:
+                    users.append(email)
             with mail.connect() as conn:
-                # for user in l1:
-                msg = Message(form.title.data,body=form.body.data,sender=(form.name.data,'automailer.0123@gmail.com'),  recipients=l1)
-                if file_name:
-                    with app.open_resource(os.getenv('ATTACHMENT_FOLDER')+file_names) as fp:
-                        mime_type, encoding = mimetypes.guess_type(file_name)
-                        msg.attach(file_name,mime_type,fp.read())     
+                # for user in users:
+                msg = Message(form.title.data,body=form.body.data,sender=(form.name.data,app.config['MAIL_USERNAME']),  recipients=users)
+                if sanitized_filepath:
+                    with app.open_emailsource(os.path.join(os.getcwd(),'mailer','static','attachments',attachment_filename)) as fp:
+                        mime_type, _ = mimetypes.guess_type(sanitized_filepath)
+                        msg.attach(sanitized_filepath,mime_type,fp.read())     
                 conn.send(msg)
             flash('Mail sent!')
-            if file_name:
-                os.remove(os.path.join(app.config['UPLOAD_FOLDER'],file_names))
+            app_logger.info("Mail sent successfully!")
+            if sanitized_filepath:
+                os.remove(os.path.join(app.config['UPLOAD_FOLDER'],attachment_filename))
             return redirect('home') 
         except Exception as e:
-            print(e)
+            app_logger.exception("Error occurred while sending mail")
             flash('Invalid file format!') 
             return redirect('home')
     return render_template('home.html',form=form)
